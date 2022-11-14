4aca1718f2e359554b7360abdd57dff2c7d5b03f
tursynkhan <tursynkhan001@gmail.com>
2022-11-14 23:18:14 +0600

    method man

--- method-man.js
+++ method-man.js
@@ -11,5 +11,5 @@ function whisper(s){
     return '*'+s.toLowerCase()+'*'
 }
 function capitalize(s){
-    return s.charAt(0).toUpperCase()+s.slice(1)
+    return s.charAt(0).toUpperCase()+s.slice(1).toLowerCase()
 }
