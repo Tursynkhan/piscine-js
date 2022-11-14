447118168e95e8f87a014d4e7d6a20b65dad559c
tursynkhan <tursynkhan001@gmail.com>
2022-11-14 23:04:10 +0600

    method man

---
+++ method-man.js
@@ -0,0 +1,15 @@
+function words(s){
+    return s.split(' ')
+}
+function sentence(s){
+    return s.join(' ')
+}
+function yell(s){
+    return s.toUpperCase()
+}
+function whisper(s){
+    return s.toLowerCase()
+}
+function capitalize(s){
+    return s.charAt(0).toUpperCase()+s.slice(1)
+}
