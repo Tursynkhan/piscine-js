4c97430ab1cde6a847acec9252fbb79deb5d90cf
tursynkhan <tursynkhan001@gmail.com>
2022-11-14 13:46:32 +0600

    declarations

---
+++ declarations.js
@@ -0,0 +1,20 @@
+const escapeStr="\`\\\/\"\'"
+const arr=[4,'2']
+const obj={
+    str:"Hello",
+    num:1,
+    bool:true,
+    undef:undefined,
+}
+const nested ={
+    arr:[4,undefined,'2'],
+    obj:{
+        str:"hi",
+        num:1,
+        bool:true,
+    }
+}
+
+Object.freeze(nested);
+Object.freeze(arr);
+Object.freeze(obj);
