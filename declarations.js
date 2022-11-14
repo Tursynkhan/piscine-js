ee92e9103e9cb4fe8c337325f667dd48d98753ae
tursynkhan <tursynkhan001@gmail.com>
2022-11-14 14:04:38 +0600

    declarations

--- declarations.js
+++ declarations.js
@@ -14,7 +14,23 @@ const nested ={
         bool:true,
     }
 }
-
-Object.freeze(nested);
-Object.freeze(arr);
+function deepFreeze(object) {
+    const propNames = Object.getOwnPropertyNames(object);
+  
+  
+    for (const name of propNames) {
+      const value = object[name];
+  
+      if (value && typeof value === "object") {
+        deepFreeze(value);
+      }
+    }
+  
+    return Object.freeze(object);
+  }
+  
 Object.freeze(obj);
+Object.freeze(arr);
+deepFreeze(nested)
+nested.obj.str="heelo"
+console.log(nested)
