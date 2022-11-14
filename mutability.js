933ea14b83ce5bb570ff720562c4bc4038fcc09d
tursynkhan <tursynkhan001@gmail.com>
2022-11-14 15:10:39 +0600

    mutability

---
+++ mutability.js
@@ -0,0 +1,18 @@
+const person = {
+    name: 'Rick',
+    age: 77,
+    country: 'US',
+  }
+  let clone1={}
+  let clone2={}
+
+  for (let key in person){
+    clone1[key]=person[key]
+  }
+  for (let key in person){
+    clone2[key]=person[key]
+  }
+  let samePerson=person
+
+    person.age=78
+    person.country='FR'
