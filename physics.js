116432c44dfae241064dc7bf597a3a2cc7c04f04
tursynkhan <tursynkhan001@gmail.com>
2022-11-15 20:30:06 +0600

    physics

--- physics.js
+++ physics.js
@@ -2,11 +2,11 @@
 function getAcceleration(obj){
     if (typeof(obj.Δv)==='number' && typeof(obj.Δt)==='number') {
         return obj.Δv/obj.Δt;
-    }else if (obj.f==='number' && obj.m==='number'){
+    }else if (typeof(obj.f)==='number' && typeof(obj.m)==='number'){
         return obj.f/obj.m
-    }else if (obj.t==='number' && obj.d==='number'){
+    }else if (typeof(obj.t)==='number' && typeof(obj.d)==='number'){
 
-        return a=(obj.d*2)/obj.t*obj.t
+        return (obj.d*2)/obj.t*obj.t
     }
     return "impossible"
 }
