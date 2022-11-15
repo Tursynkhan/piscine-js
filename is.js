4a38f7018a87f26d1ffc46c0c34272a478b4ac33
tursynkhan <tursynkhan001@gmail.com>
2022-11-15 19:14:24 +0600

    is.js

--- is.js
+++ is.js
@@ -1,43 +1,46 @@
 is.num=function (n){
 
-    return typeof(n)==='number'
+    return typeof(n)==='number';
 }
 is.nan=function(n){
     if (Number.isNaN(n)){
-        return true
+        return true;
     }
-    return false
+    return false;
 }
 is.str=function (str){
     if (typeof(str)==='string'){
-        return true
+        return true;
     }
-    return false
+    return false;
 }
 is.bool=function(bool){
     if (typeof(bool)==='boolean'){
-        return true
+        return true;
     }
-    return false
+    return false;
 }
 is.undef=function (undef){
     if (typeof(undef)==='undefined'){
-        return true
+        return true;
     }
-    return false
+    return false;
 }
 is.def=function (def){
     if (typeof(def)!=='undefined'){
-        return true
+        return true;
     }
-    return false
+    return false;
 }
 is.arr=function (arr){
-    return Array.isArray(arr)
-   
+    return Array.isArray(arr);
 }
 is.obj=function (obj){
-    if (typeof(obj)==='object'){
+    if (
+        typeof obj === 'object' &&
+        !Array.isArray(obj) &&
+        obj !== null
+    ){
         return true
     }
     return false
