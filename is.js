10e54e94b9a2b1c8707efa83fc39be0b0996ae73
tursynkhan <tursynkhan001@gmail.com>
2022-11-15 17:41:54 +0600

    is.js

--- is.js
+++ is.js
@@ -34,15 +34,8 @@ is.def=function (def){
     return false
 }
 is.arr=function (arr){
-    if (typeof(arr)==='object'){
+    if (isArray(arr)){
         return true
-    }
-    for (let i=0;i<arr.length;i++){
-        
-        if (typeof(arr)==='object'){
-            return true
-        }
-        return false
     }
     return false
 }
