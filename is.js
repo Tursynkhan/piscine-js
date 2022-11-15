3334f7b62fd9ee4b030d40aefa79e61b45f53cb2
tursynkhan <tursynkhan001@gmail.com>
2022-11-15 17:16:28 +0600

    is.js

--- is.js
+++ is.js
@@ -1,66 +1,63 @@
 
-function num(n){
-    if (typeof(n)==='number'){
-        return true
-    }
-    return false
-    
+is.num=function (n){
+
+    return typeof(n)==='number'
 }
-function nan(n){
+is.nan=function(n){
     if (Number.isNaN(n)){
         return true
     }
     return false
 }
-function str(str){
+is.str=function (str){
     if (typeof(str)==='string'){
         return true
     }
     return false
 }
-function bool(bool){
+is.bool=function(bool){
     if (typeof(bool)==='boolean'){
         return true
     }
     return false
 }
-function undef(undef){
+is.undef=function (undef){
     if (typeof(undef)==='undefined'){
         return true
     }
     return false
 }
-function def(def){
+is.def=function (def){
     if (typeof(def)!=='undefined'){
         return true
     }
     return false
 }
-function arr(arr){
+is.arr=function (arr){
     if (typeof(arr)==='object'){
         return true
     }
     return false
 }
-function obj(obj){
+is.obj=function (obj){
     if (typeof(obj)==='object'){
         return true
     }
     return false
 }
-function fun(f){
+is.fun=function (f){
     if (typeof(f)==='function'){
         return true
     }
     return false
 }
-function truthy(truthy){
+is.truthy=function (truthy){
     if (truthy===true){
         return true
     }
     return false
 }
-function falsy(falsy){
+is.falsy=function (falsy){
     if (falsy===false){
         return false
     }
