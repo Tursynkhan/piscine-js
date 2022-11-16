16118da568cfa8991d4b7b8f8e3fd5615b2aaf8c
tursynkhan <tursynkhan001@gmail.com>
2022-11-16 22:29:27 +0600

    blockchain

--- block-chain.js
+++ block-chain.js
@@ -1,22 +1,10 @@
-function Block(index, hash, data, prev, chain) {
-    this.index = index;
-    this.hash = hash;
-    this.data = data;
-    this.prev = prev;
-    this.chain = chain;
-  }
-  
-  const blockChain = (data, prev = { index: 0, hash: '0' }) => {
-    var block = new Block
-      if (prev['index'] === 0){
-        block.index = 1
-      }else{
-          block.index = prev['index'] + 1
-      }
-      block.hash = hashCode(block.index + prev['hash'] + JSON.stringify(data))
-      block.data = data
-      block.prev = prev
-      block.chain = (e) => blockChain(e, {index: block.index, hash: block.hash})
-      return block
+function blockChain(data,prev = { index: 0, hash: '0' }){
+    let block  = {
+        data,
+        prev,
     }
-  
+    block.index = block.prev.index + 1
+    block.hash = hashCode(block.index+prev.hash+JSON.stringify(data))
+    block.chain = (data) => blockChain(data,block)
+  return block
+}
