8fb119eb182814ada396518ee7e9585bb6bc008e
tursynkhan <tursynkhan001@gmail.com>
2022-11-16 19:09:17 +0600

    collections

--- collections.js
+++ collections.js
@@ -1,17 +1,3 @@
-const str = 'hello'
-const arr = [1, 2, 1, 3]
-const obj = { x: 45, y: 75, radius: 24 }
-const set = new Set()
-const map = new Map()
-set.add(1)
-set.add(2)
-set.add(1)
-set.add(3)
-map.set('a', 1)
-map.set('b', 2)
-map.set(3, 'c')
-map.set(4, 'd')
-
 function arrToSet(arr){
     const set= new Set(arr)
     return set 
@@ -29,7 +15,7 @@ function setToStr(set){
 }
 function strToArr(str){
     const arr= str.split('')
-    return new Set(arr)
+    return arr
 }
 function strToSet(str){
     const arr=str.split('')
@@ -43,7 +29,7 @@ function objToMap(obj){
     return map
 }
 function objToArr(obj){
-    return Object.entries(obj)
+    return Object.values(obj)
 }
 function arrToObj(arr){
     return Object.assign({},arr)
