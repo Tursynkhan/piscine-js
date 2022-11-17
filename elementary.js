b11ca650cf4cf1c415b3267a75a435bc15af2e26
tursynkhan <tursynkhan001@gmail.com>
2022-11-17 14:28:14 +0600

    elementary

--- elementary.js
+++ elementary.js
@@ -1,9 +1,22 @@
 function multiply(a,b){
-    return a*=b
+    let res=0
+    for (let i=0;i<b;i++){
+        res+=a
+    }
+    return res
 }
 function divide(a,b){
-    return a/=b
+    let count=0;
+    let r=a;
+    while(r>=b){
+        r=r-b;
+        count++
+    }
+    return count;
 }
 function modulo(a,b){
-    return a%=b
+    while(b<a){
+        a=a-b
+    }
+    return a
 }
