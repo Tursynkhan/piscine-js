ad4207a4227b357632ea1d66dcfcbfd890418f26
tursynkhan <tursynkhan@noreply.example.org>
2022-11-18 20:55:24 +0000

    get

---
+++ get.js
@@ -0,0 +1,10 @@
+
+const get = (src, path) => {
+    let pathArr = path.split('.')
+    let cur = src
+    pathArr.forEach((element) => {
+        if (cur === undefined) return cur
+        cur = cur[element]
+    })
+    return cur
+}
