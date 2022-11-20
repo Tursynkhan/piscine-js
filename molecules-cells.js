0b5955a288e2b26a1f7d2e1e1b82566f1bb91911
tursynkhan <tursynkhan001@gmail.com>
2022-11-20 20:17:30 +0600

    molecules

--- molecules-cells.js
+++ molecules-cells.js
@@ -1,36 +1,43 @@
-function DNA(RNA){
-    let res = ''
-    for(let i = 0;i<RNA.length;i++){
-        if(RNA[i] === 'C'){
-            res += 'G'
-        }
-        if(RNA[i] === 'G'){
-            res += 'C'
-        }
-        if(RNA[i] === 'A'){
-            res += 'T'
-        }
-        if(RNA[i] === 'U'){
-            res += 'A'
+function RNA(str){
+    let res=''
+    if (str.length==0){
+        return res
+    }
+    for(let i=0;i<str.length;i++){
+        if (str[i]=='G'){
+            res+='C'
+        }else if (str[i]=='C'){
+            res+='G'
+        }else if(str[i]=='T'){
+            res+='A'
+        }else if(str[i]=='A'){
+            res+='U'
+        }else{
+
+            res+=str[i]
         }
     }
     return res
 }
-function RNA(DNA){
-    let res = ''
-    for(let i = 0;i<DNA.length;i++){
-        if(DNA[i] === 'G'){
-            res += 'C'
-        }
-        if(DNA[i] === 'C'){
-            res += 'G'
-        }
-        if(DNA[i] === 'T'){
-            res += 'A'
-        }
-        if(DNA[i] === 'A'){
-            res += 'U'
+function DNA(str){
+    let res=''
+    if (str.length==0){
+        return res
+    }
+    for(let i=0;i<str.length;i++){
+        if (str[i]=='C'){
+            res+='G'
+        }else if (str[i]=='G'){
+            res+='C'
+        }else if(str[i]=='A'){
+            res+='T'
+        }else if(str[i]=='U'){
+            res+='A'
+        }else{
+
+            res+=str[i]
         }
     }
     return res
 }
+console.log(DNA('AUCG'))
