ae40cea691404e28e1b9ae4e500a54722c8bf56c
tursynkhan <tursynkhan001@gmail.com>
2022-11-20 22:44:16 +0600

    get

--- get.js
+++ get.js
@@ -1,10 +1,22 @@
-
-const get = (src, path) => {
-    let pathArr = path.split('.')
-    let cur = src
-    pathArr.forEach((element) => {
-        if (cur === undefined) return cur
-        cur = cur[element]
-    })
-    return cur
+function get(src, path){
+    let arrPath=path.split('.')
+    let res
+    for (let i = 0; i < arrPath.length; i++){
+        if ( res ){
+            res = res[arrPath[i]]
+        }else{
+            res = src[arrPath[i]]
+        }
+    }
+    console.log(res)
 }
+// const get = (src, path) => {
+//     let pathArr = path.split('.')
+//     let cur = src
+//     pathArr.forEach((element) => {
+//         if (cur === undefined) return cur
+//         cur = cur[element]
+//     })
+//     return cur
+// }
+get({nested:{ key: {another: 'value'} }}, 'nested.key.another')
