bb64fb46cae22ca27c4c2f38fc01e6d23a6bb272
tursynkhan <tursynkhan001@gmail.com>
2022-11-20 18:41:17 +0600

    chunky

--- chunky.js
+++ chunky.js
@@ -1,13 +1,5 @@
-
-function chunk(arr,int){
-    let res = []
-    for(let i =0;i<arr.length;i++){
-        res.push(arr.slice(0,int))
-        arr = arr.slice(int)
-        if(arr.length<int && arr.length != 0){
-            res.push(arr)
-            return res
-        }
-    }
+function chunk(arr,size){
+    let res=[];
+    res.push(arr.slice(0,size),arr.slice(size)) 
     return res
 }
