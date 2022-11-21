7808c43376b37b53bdcca7f2a8008a30acaf4209
tursynkhan <tursynkhan001@gmail.com>
2022-11-21 18:23:16 +0600

    date-is

--- date-is.js
+++ date-is.js
@@ -1,42 +1,44 @@
-function isValid(d){
-    if(isNaN(d)|| d == 0){
+function isValid(date){
+    if(isNaN(date)|| date == 0){
         return false
     }else{
         return true
     }
 }
 function isAfter(date1,date2){
-    let d1=date1.getTime()
-    let d2=date2.getTime()
-    if (d1>d2){
+    let res = date1 - date2
+    if(res>0){
         return true
     }else{
         return false
     }
 }
 function isBefore(date1,date2){
-    let d1=date1.getTime()
-    let d2=date2.getTime()
-    if (d1<d2){
+    let res = date1 - date2
+    if(res<0){
         return true
     }else{
         return false
     }
 }
-function isFuture(date){
-    let dateNow=Date.now()
-    if (date>dateNow){
+function isFuture(date1){
+    let date2 = new Date()
+    let res = date1 - date2
+    if(res>0){
         return true
     }else{
         return false
     }
 }
-function isPast(date){
-    let dateNow=Date.now()
-    if (date<dateNow){
+function isPast(date1){
+    if(!isValid(date1)){
+        return false
+    }
+    let date2 = new Date()
+    let res = date1 - date2
+    if(res<0){
         return true
     }else{
         return false
     }
 }
-console.log(isValid(Date.now()))
