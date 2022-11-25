9c84c5295aecd82c3c9831f7836615a55aba7842
tursynkhan <tursynkhan001@gmail.com>
2022-11-25 20:43:57 +0600

    call-me-maybe

---
+++ call-me-maybe/using-filter.js
@@ -0,0 +1,93 @@
+function filterShortStateName(arr){
+    const result=arr.filter(word=>word.length<7)
+    return result
+}
+function filterStartVowel(arr){
+    function vowel(word){
+        let vowel=['e','y','u','i','o','a','E','Y','U','I','O','A']
+        for (let i=0;i<vowel.length;i++){
+            if (vowel[i]==word[0]){
+                return word
+            }
+        }
+    };
+    return arr.filter(vowel)
+}
+function filter5Vowels(arr){
+    const result=arr.filter(word=>{
+        let count=0;
+        let vowels=['a','e','i','o','u'];
+        for (let  i=0;i<word.length;i++){
+                for (let j=0;j<vowels.length;j++){
+                if (word[i]==vowels[j]){
+                    count++
+                    break
+                }
+            }
+        }
+        if (count>=5){
+            return word
+        }
+    });
+    return result
+}
+function filter1DistinctVowel(arr){
+        const  result=arr.filter(word=>{
+        let count=0
+        let vowels=['a','A','e','E','o','O','i','I','u','U']
+        for (let j=0;j<vowels.length;j++){
+            for (let  i=0;i<word.length;i++){
+                if (word[i]==vowels[j]){
+                    if (j%2==0){
+                        j=j+1
+                    }
+                    count++
+                    break
+                }
+            }
+        }
+        if (count==1){
+            return word
+        }
+    });
+    return result
+}
+function multiFilter(arr){
+    let result=arr.filter(word=>{
+        
+        let flagCapital = false 
+        let flagName = false
+        let flagTag = false
+        let flagRegion = false
+        if(word.capital.length>=8){
+            flagCapital = true
+        }
+        let vowels=['a','e','o','i','u','y','A','E','O','I','U']
+        let name=word.name
+        for (let i=0;i<vowels.length;i++){
+            if (name[0]===vowels[i]){
+                flagName = true
+            }
+        }
+        let tag=word.tag
+        let count=0;
+        for (let  i=0;i<tag.length;i++){
+                for (let j=0;j<vowels.length;j++){
+                if (tag[i]==vowels[j]){
+                    count++
+                    break
+                }
+            }
+        }
+        if (count==1){
+            flagTag=true
+        }
+        if (word.region!="South"){
+            flagRegion=true
+        }
+        if (flagCapital && flagName===false && flagTag && flagRegion){
+            return word
+        } 
+    });
+    return result
+}
---
+++ call-me-maybe/using-map.js
@@ -0,0 +1,38 @@
+function citiesOnly(arr){
+    return arr.map((key)=>key.city)
+}
+function upperCasingStates(arr){
+    const temp=arr.map(key=>{
+        const arr2=key.split(" ");
+        for (let i=0;i<arr2.length;i++){
+            arr2[i]=arr2[i].charAt(0).toUpperCase()+arr2[i].slice(1);
+        }
+        const str2=arr2.join(" ");
+        return str2
+    });
+    return temp
+}
+function fahrenheitToCelsius(arr){
+    return arr.map(key=>Math.floor(((parseInt(key)-32)*5)/9)+'°C')
+}
+function trimTemp(arr) {
+    const temp = arr.map((key) =>{
+        key.temperature = key.temperature.replace(/ /g, '');
+        return key
+    });
+    return temp
+}
+
+function tempForecasts(arr){
+    const str=arr.map((all)=>{
+        let temp=Math.floor(((parseInt(all.temperature)-32)*5)/9)+'°Celsius';
+        let city=all.city;
+        const arr2=all.state.split(" ");
+        for (let i=0;i<arr2.length;i++){
+            arr2[i]=arr2[i].charAt(0).toUpperCase()+arr2[i].slice(1);
+        }
+        const state=arr2.join(" ");
+        return temp+' in '+city+', '+state
+    });
+    return str
+}
