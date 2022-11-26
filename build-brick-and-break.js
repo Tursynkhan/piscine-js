a6cc405e6f6a6256f83da9804c9d03e56363094d
tursynkhan <tursynkhan001@gmail.com>
2022-11-26 20:30:37 +0600

    build-brick

--- build-brick-and-break.js
+++ build-brick-and-break.js
@@ -1,20 +1,29 @@
 export function build(number){
     let i=1;
+    let counter=2;
     let interval = setInterval(() => {
         let div=document.createElement("div")
         document.body.append(div)
         div.innerHTML=i
         div.id="brick-"+i
         i++
-        if (number === i) {
+        if (counter==3){
+            div.dataset.foundation=true
+            counter=0
+        }
+        counter++
+        if (number+1 === i) {
             clearInterval(interval)
         }
-    }, 100);
+    }, 100,i);
 }
 
 
-export function repair() {
-
+export function repair(...ids) {
+    for (let i=0;i<ids.length;i++){
+    }
 }
 
-export function destroy() {}
+export function destroy() {
+
+}
