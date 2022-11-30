9b47ab3397eaa7b979a0e936e3926c57d894fd03
tursynkhan <tursynkhan001@gmail.com>
2022-11-30 19:44:59 +0600

    pick-omit

--- pick-omit.js
+++ pick-omit.js
@@ -19,14 +19,12 @@ function omit(obj,keys){
     const newObj={}
     for (let key in obj){
         if (typeof(keys)==='string'){
-            if (keys!=key){
+            if (key!==keys){
                 newObj[key]=obj[key]
             }
         }else if(typeof(keys)==='object'){
-            for (let i=0;i<keys.length;i++){
-                if (keys[i]!=key){
-                    newObj[key]=obj[key]
-                }
+            if(keys.includes(key)!=true){
+                newObj[key]=obj[key]
             }
         }
     }
