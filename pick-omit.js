8b8cddecb33ff6e19515b51f31bc77993b8379b6
tursynkhan <tursynkhan001@gmail.com>
2022-11-30 20:07:16 +0600

    pick-omit

--- pick-omit.js
+++ pick-omit.js
@@ -18,15 +18,19 @@ function pick(obj,keys){
 function omit(obj,keys){
     const newObj={}
     for (let key in obj){
-        if (typeof(keys)==='string'){
-            if (key!==keys){
-                newObj[key]=obj[key]
-            }
-        }else if(typeof(keys)==='object'){
-            if(keys.includes(key)!=true){
-                newObj[key]=obj[key]
+        if (obj.hasOwnProperty(key)){
+
+            if (typeof(keys)==='string'){
+                if (key!==keys){
+                    newObj[key]=obj[key]
+                }
+            }else if(typeof(keys)==='object'){
+                if(!(keys.includes(key))){
+                    newObj[key]=obj[key]
+                }
             }
         }
     }
     return newObj
 }
+console.log(omit({ something: 5, __proto__: { d: 6 } }, ['proto', 'something']))
