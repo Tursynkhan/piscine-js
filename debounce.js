943cbb6cd215db6275ca6aef58d94aaedf3e2864
tursynkhan <tursynkhan001@gmail.com>
2022-12-01 20:07:46 +0600

    debounce

--- debounce.js
+++ debounce.js
@@ -1,39 +1,27 @@
-const debounce = (func,delay)=>{
-    let timeout=null
-    return (...args)=>{
-        if (timeout!==null){
-            clearTimeout(timeout)
-            timeout=null
-        }
-        timeout=setTimeout(()=>{
-            func(...args)
-        },delay)
-    }
+// make the last action after delay
+const debounce = (func, delay) => {
+    let timeout;
+    return (...args) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            func.apply(this, args);
+        }, delay);
+    };
 }
-const opDebounce=(func,delay,options)=>{
-    const leading =(options.leading) ? true : false
-    let timeout=null
-    let cooldown=false
-    return (...args)=>{
-        if (leading){
-            if(timeout!==null){
-                clearTimeout(timeout)
-                timeout=null
-            }
-            timeout=setTimeout(()=>{
-                cooldown=false
-            },delay)
-            if (cooldown) return 
-            func(...args)
-            cooldown=true
-        }else{
-            if(timeout!==null){
-                clearTimeout(timeout)
-                timeout=null
-            }
-            timeout=setTimeout(()=>{
-                func(...args)
-            },delay)
-        }
+// leading = false - make the last action after delay
+// leading = true - make at start only
+const opDebounce = (func, delay, leading) => {
+    if (!leading) {
+        return debounce(func, delay);
     }
+    let timeout;
+    return (...args) => {
+        if (!timeout) {
+            func.apply(this, args);
+        }
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+           timeout = undefined;
+        }, delay);
+    };
 }
