9663ad8d5022034d0e4c318b0f0d885538947704
tursynkhan <tursynkhan001@gmail.com>
2023-06-12 19:24:42 +0600

    mutability

--- mutability.js
+++ mutability.js
@@ -16,17 +16,3 @@ for (let key in person){
 }
 
 
-t(() => typeof samePerson === 'object')
-t(() => typeof person === 'object')
-t(() => typeof clone1 === 'object')
-t(() => typeof clone2 === 'object')
-t(({ eq }) => eq(clone1, clone2)) // equal
-t(() => clone1 !== clone2) // but different !
-t(() => person === samePerson) // same value
-t(() => person.name === 'Rick')
-t(() => person.age === 78)
-t(() => person.country === 'FR')
-t(() => clone1.country === 'US')
-t(() => clone2.age === 77)
-
-Object.freeze(tests)
