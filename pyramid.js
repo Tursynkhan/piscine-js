5be0e8579bf4da8efd0c1a4fdf0f51b32a4e08b1
tursynkhan <tursynkhan001@gmail.com>
2023-06-19 21:18:54 +0600

    pyramid

---
+++ pyramid.js
@@ -0,0 +1,16 @@
+const pyramid=(n)=>{
+    res=''
+    for (let i=1;i<=n;i++){
+        for(let j=1;j<=n*2-1;j++){
+            if(j>n-i && j<=n-1+i){
+                res+='*'
+            }else{
+                res+=' '
+            }
+        }
+        res+='\n'
+    }
+    return res.slice(0,res.length-1)
+}
+
+console.log(pyramid(3))
