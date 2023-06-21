9fb3943c1a8d70747828c537ced0769945be9ed0
tursynkhan <tursynkhan001@gmail.com>
2023-06-21 15:47:18 +0600

    pyramid

--- loop/pyramid.js
+++ loop/pyramid.js
@@ -12,3 +12,22 @@ function pyramid(str, int) {
     return res
 }
 console.log(pyramid('{}', 12))
+
+// const pyramid=(sep,n)=>{
+//     let res=''
+//     for (let i=1;i<=n;i++){
+//         for(let j=1;j<=n-1+i;j++){
+//             if(j>n-i){
+//                 res+=sep
+//             }else if(sep.length==3){
+//                 res+='   '
+//             }else if (sep.length==2){
+//                 res+='  '
+//             }else if (sep.length==1){
+//                 res+=' '
+//             }
+//         }
+//         res+='\n'
+//     }
+//     return res.slice(0,res.length-1)
+// }
