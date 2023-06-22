7697dfead0052135b7cfba2701c7a1d729d65af3
tursynkhan <tursynkhan001@gmail.com>
2023-06-22 19:30:54 +0600

    letter-space-number

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,5 +1,6 @@
 const letterSpaceNumber=(str)=>{
-    let result =str.match(/[a-zA-Z]\s\d/g);
-    return result
+    const regex = /[a-zA-Z]\s(\d)(?!\d|\w)/g;
+    const matches = str.match(regex);
+    return matches ? matches : [];
 }
 console.log(letterSpaceNumber('He is 8 or 9 years old, not 10.'))
